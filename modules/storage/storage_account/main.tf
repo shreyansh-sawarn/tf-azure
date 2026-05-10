ec2ff4c87323a3b119aa94ef2a2812eaee96d5be
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2026-05-11 01:21:52 +0530

    fix fmt

--- modules/storage/storage_account/main.tf
+++ modules/storage/storage_account/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_storage_account" "storage" {
-  name                       = var.name
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  account_tier               = var.account_tier
-  account_replication_type   = var.replication_type
-  https_traffic_only_enabled = true
-  min_tls_version            = "TLS1_2"
+  name                          = var.name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  account_tier                  = var.account_tier
+  account_replication_type      = var.replication_type
+  https_traffic_only_enabled    = true
+  min_tls_version               = "TLS1_2"
   public_network_access_enabled = var.public_network_access_enabled
-  tags                       = var.tags
+  tags                          = var.tags
 }
 
 resource "azurerm_storage_container" "containers" {
