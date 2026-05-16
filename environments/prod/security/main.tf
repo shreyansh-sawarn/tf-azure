13244f53087638f7a291dca6e5681628176bb815
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2026-05-16 10:00:30 +0530

    minor fixes

--- environments/prod/security/main.tf
+++ environments/prod/security/main.tf
@@ -23,7 +23,7 @@ module "diag_keyvault" {
 
   name               = "diag-keyvault"
   target_resource_id = module.key_vault.id
-  log_analytics_workspace_id = var.log_analytics_id
+  workspace_id       = var.log_analytics_id
 }
 
 module "recovery_vault" {
