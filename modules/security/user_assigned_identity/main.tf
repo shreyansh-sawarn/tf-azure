f8593cc2f6181654b0d20fbb07e4df7182ca3371
Shreyansh Sawarn <shreyanshsawarn88@gmail.com>
2026-05-17 00:33:25 +0530

    remove duplicate blocks

--- modules/security/user_assigned_identity/main.tf
+++ modules/security/user_assigned_identity/main.tf
@@ -4,15 +4,3 @@ resource "azurerm_user_assigned_identity" "identity" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
-
-output "id" {
-  value = azurerm_user_assigned_identity.identity.id
-}
-
-output "principal_id" {
-  value = azurerm_user_assigned_identity.identity.principal_id
-}
-
-output "client_id" {
-  value = azurerm_user_assigned_identity.identity.client_id
-}
